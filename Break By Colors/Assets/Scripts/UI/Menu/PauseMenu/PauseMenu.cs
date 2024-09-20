e662b7b89d1959743a6e2a22dbd1c83cf59dc7c3
banana21218 <113222030+banana21218@users.noreply.github.com>
2024-09-19 18:53:03 -0700

    Prefabs&PauseMenu
    created Wall Prefabs and created PauseMenu Scipt.(not implemented yet)

---
+++ Break By Colors/Assets/Scripts/UI/Menu/PauseMenu/PauseMenu.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject QuitButton;
+    public GameObject RestartButton;
+    public TextMeshProUGUI Paused;
+    public bool MenuUp = false;
+
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        QuitButton.SetActive(false);
+        RestartButton.SetActive(false);
+        MenuUp = false;
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        QuitButton.SetActive(true);
+        RestartButton.SetActive(true);
+        MenuUp = true;
+        Time.timeScale = 0f;
+        Debug.Log("bruh");
+        GameIsPaused = true;
+        Paused.text = "Paused";
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void sceneSwitch()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+}
